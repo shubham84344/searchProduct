dc8a8fd387542a611e23d54f5a9a2f79365009f5
Shubham Patel <pshubham84344@gmail.com>
2025-07-23 15:19:12 +0530

    changes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductList.scss';
 
 function ProductList() {
-  return (
-    <div className='ProductListScss'>
-      
-    </div>
-  )
+
+    const [category, setCategory] = useState([]);
+    console.log('category', category)
+
+    useEffect(() => {
+        async function categoriesApiCall() {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products/categories");
+                if (!response.ok) {
+                    console.log('Response not OK', response.status);
+                    return;
+                }
+                const data = await response.json();
+                setCategory(data);
+            } catch (error) {
+                console.log('Error fetching categories:', error);
+            }
+        }
+        categoriesApiCall();
+    }, []);
+
+    return (
+        <div className='ProductListScss'>
+            <div className='leftSide'>
+                <ul>
+                    {category.map((list, index) => (
+                        <li key={index}>{list}</li>
+                    ))}
+                </ul>
+            </div>
+            <div className=' rightSide'>
+                <p>right side</p>
+            </div>
+        </div>
+    )
 }
 
 export default ProductList
